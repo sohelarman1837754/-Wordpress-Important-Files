74ffcb8e597ec170fa223f390732b36f984ade52
sohelarman1837754 <41370854+sohelarman1837754@users.noreply.github.com>
2020-03-31 20:57:52 +0600

    Create ajax-live-search.php

---
+++ ajax-live-search.php
@@ -0,0 +1,46 @@
+//For despling the form and result
+<input type="text" name="keyword" id="keyword" onkeyup="fetch()"></input>
+
+<div id="datafetch">Search results will appear here</div>
+
+
+//Ajax search
+// add the ajax fetch js
+add_action( 'wp_footer', 'ajax_fetch' );
+function ajax_fetch() {
+?>
+<script type="text/javascript">
+function fetch(){
+
+    jQuery.ajax({
+        url: '<?php echo admin_url('admin-ajax.php'); ?>',
+        type: 'post',
+        data: { action: 'data_fetch', keyword: jQuery('#keyword').val() },
+        success: function(data) {
+            jQuery('#datafetch').html( data );
+        }
+    });
+
+}
+</script>
+
+<?php
+}
+
+// the ajax function
+add_action('wp_ajax_data_fetch' , 'data_fetch');
+add_action('wp_ajax_nopriv_data_fetch','data_fetch');
+function data_fetch(){
+
+    $the_query = new WP_Query( array( 'posts_per_page' => -1, 's' => esc_attr( $_POST['keyword'] ), 'post_type' => 'product' ) );
+    if( $the_query->have_posts() ) :
+        while( $the_query->have_posts() ): $the_query->the_post(); ?>
+
+            <h2><a href="<?php echo esc_url( post_permalink() ); ?>"><?php the_title();?></a></h2>
+            
+        <?php endwhile;
+        wp_reset_postdata();  
+    endif;
+
+    die();
+}
