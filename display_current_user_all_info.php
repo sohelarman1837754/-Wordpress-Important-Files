1093a1a2eafc5d01b70defcb8451cbd2ae14259d
sohelarman1837754 <41370854+sohelarman1837754@users.noreply.github.com>
2020-06-17 14:32:12 +0600

    Create display_current_user_all_info.php

---
+++ display_current_user_all_info.php
@@ -0,0 +1,18 @@
+<?php
+
+$current_user = wp_get_current_user();
+ 
+/*
+ * @example Safe usage: $current_user = wp_get_current_user();
+ * if ( ! ( $current_user instanceof WP_User ) ) {
+ *     return;
+ * }
+ */
+printf( __( 'Username: %s', 'textdomain' ), esc_html( $current_user->user_login ) ) . '<br />';
+printf( __( 'User email: %s', 'textdomain' ), esc_html( $current_user->user_email ) ) . '<br />';
+printf( __( 'User first name: %s', 'textdomain' ), esc_html( $current_user->user_firstname ) ) . '<br />';
+printf( __( 'User last name: %s', 'textdomain' ), esc_html( $current_user->user_lastname ) ) . '<br />';
+printf( __( 'User display name: %s', 'textdomain' ), esc_html( $current_user->display_name ) ) . '<br />';
+printf( __( 'User ID: %s', 'textdomain' ), esc_html( $current_user->ID ) );
+
+$items.=''.get_avatar( $current_user->ID, 32 ).'';
