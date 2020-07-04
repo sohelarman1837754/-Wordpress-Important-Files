f840c68e345b042e4e6779bdcf3f4d81a32f04ef
sohelarman1837754 <41370854+sohelarman1837754@users.noreply.github.com>
2020-07-04 13:37:03 +0600

    Update Remove_Or_Add_Dashboard_Widgets.php

--- Remove_Or_Add_Dashboard_Widgets.php
+++ Remove_Or_Add_Dashboard_Widgets.php
@@ -23,14 +23,17 @@ add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
 
 //For adding a new Widget
 
-function wpdocs_add_dashboard_widgets() {
-    wp_add_dashboard_widget( 'dashboard_widget', 'Example Dashboard Widget', 'dashboard_widget_function' );
+function another_add_dashboard_widgets() {
+   add_meta_box( 'id', 'Dashboard Widget Title', 'dash_widget', 'dashboard', 'side', 'high' );
 }
-add_action( 'wp_dashboard_setup', 'wpdocs_add_dashboard_widgets' );
- 
+add_action( 'wp_dashboard_setup', 'another_add_dashboard_widgets' );
+
 /**
  * Output the contents of the dashboard widget
  */
-function dashboard_widget_function( $post, $callback_args ) {
-    esc_html_e( "Hello World, this is my first Dashboard Widget!", "textdomain" );
+function dash_widget( $post, $callback_args ) {
+    $html='This is a Another widget. <a href="https://google.com">Contact the outhor</a>';
+
+    echo $html;
 }
+
